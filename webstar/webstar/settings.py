370dca921dcc6d31f3a3bcc2d74ca3d4e1827335
Ernesto Celis <ernesto@celisdelafuente.net>
2024-12-19 15:01:09 -0600

    chore(tests): add websockets tests

--- webstar/webstar/settings.py
+++ webstar/webstar/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {"NAME": BASE_DIR / "test.sqlite3"},
     }
 }
 
