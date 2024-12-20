c8fa1fae8697a762a9408bb46ab8143edbc164ee
Ernesto Celis <ernesto@celisdelafuente.net>
2024-12-20 13:32:00 -0600

    chore(editor): send text among instancess of quill

--- api/editor/consumers.py
+++ api/editor/consumers.py
@@ -15,22 +15,21 @@ class EditorConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
+        print(f"Bye! {self.room_group_name}")
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        # Send message to room group
+        # message = text_data_json["message"]
+        # # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "editor.message", "message": message}
+            self.room_group_name, {"type": "editor.message", "message": text_data}
         )
 
     # Receive message from room group
     async def editor_message(self, event):
         message = event["message"]
-        print(message)
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
--- web/src/components/TextEditor.tsx
+++ web/src/components/TextEditor.tsx
@@ -1,7 +1,7 @@
 import Quill, { Delta } from "quill";
 import { useCallback, useEffect, useState } from "react";
 
-function sendText(text: string) {
+function sendText(text: string): string {
   const msg = {
     type: "message",
     text: text,
@@ -15,35 +15,11 @@ function sendText(text: string) {
   // Blank the text input element, ready to receive the next line of text from the user.
   // document.getElementById("text").value = "";
 }
+
+
 const TextEditor = function () {
   const [client, setClient] = useState<WebSocket | null>(null);
   const [quill, setQuill] = useState<Quill | null>(null);
-
-  useEffect(() => {
-    if (client === null || quill || null) return;
-
-    const ws = new WebSocket("ws://localhost:8000/ws/editor/lobby/");
-    ws.addEventListener("error", console.error);
-    setClient(ws);
-    // return () => {
-    //   ws.close();
-    // };
-  }, []);
-
-  // useEffect(() => {
-  //   console.log('lege', client, quill)
-  //   if (client === null || quill === null) return;
-
-  //   const onTextChange = (delta: Delta, oldDelta: Delta, source: string) => {
-  //     console.log(delta);
-  //     if (source !== "user") return;
-  //     client?.send(JSON.stringify(delta));
-  //   };
-    
-  //   quill?.on("text-change", onTextChange);
-
-  //   // return () => { quill?.off("text-change", onTextChange)}
-  // }, [client, quill]);
 
   const editorRef = useCallback((container: HTMLDivElement) => {
     if (container === null) return;
@@ -53,7 +29,43 @@ const TextEditor = function () {
     setQuill(new Quill(editor, { theme: "snow" }));
   }, []);
 
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8000/ws/editor/lobby/");
+    ws.addEventListener("error", console.error);
+    // ws.addEventListener("message", onMessage)
+    setClient(ws);
+    // return () => {
+    //   ws.close();
+    // };
+  }, []);
+
+useEffect(() => {
+    if (quill === null) return;
+
+    const onTextChange = (delta: Delta, oldDelta: Delta, source: string) => {
+      if (source !== "user") return;
+      
+      client?.send(JSON.stringify(delta));
+    };
+    
+    quill?.on("text-change", onTextChange);
+
+    return () => { quill?.off("text-change", onTextChange)}
+  }, [quill]);
+
+  useEffect(() => {
+    const onMessage = (event: MessageEvent<any>) => {
+      const {message} = JSON.parse(event.data);
+      const delta: Delta = JSON.parse(message) as Delta;
+      quill?.updateContents(delta);
+    }
+    // @ts-ignore
+    if(client) client.addEventListener("message", onMessage);
+  }, [quill]);
+
   return <div id="editor" ref={editorRef}></div>;
 };
+
+
 
 export default TextEditor;
